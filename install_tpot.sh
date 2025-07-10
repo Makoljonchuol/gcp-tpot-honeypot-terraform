1e85b0aecd009503533c0c6eb5894d6cddc13542
Makol J.Chuol <jchuol@macalester.edu>
2025-07-10 15:32:11 -0500

    better script

--- install_tpot.sh
+++ install_tpot.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-apt-get update && apt-get install -y git curl
+
+# Update system and install prerequisites
+sudo apt-get update
+sudo apt-get install -y git curl
+
+# Clone the official T-Pot Community Edition repository
 git clone https://github.com/telekom-security/tpotce.git
-cd tpotce/iso/installer/
-chmod +x install.sh
-# Install full T-Pot package (includes ELK)
-./install.sh --type=auto
+
+# Change to the T-Pot directory
+cd tpotce
+
+# Start the interactive installer (do NOT use sudo here)
+./install.sh
